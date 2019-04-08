9a647bc6a85f8ddcd57ca428c41b813dcd618633
ASUS <ASUS@DESKTOP-DV9PLLJ>
2019-04-08 18:05:23 +0800

    2019-4-8 afternoon

--- src/index.js
+++ src/index.js
@@ -2,11 +2,18 @@
 import ReactDOM from 'react-dom'
 import App from './App.js'
 import Store from './stores/Store'
+import TodoStore from './stores/TodoStore'
 
 import Todo from './view/todo/todo';
 import Index from './view/Index';
+import RouterVar from './contants/Router'
+import { LocaleProvider } from 'antd';
+import { Provider } from 'mobx-react';
+import zh_CN from 'antd/lib/locale-provider/zh_CN';
+import 'moment/locale/zh-cn';
 
 let store = new Store();
+let todoStore = new TodoStore();
 import {
     BrowserRouter as Router,
     Route,
@@ -14,11 +21,15 @@ import {
 import TodoForm from './view/todo/TodoForm.js';
 
 ReactDOM.render(
-    <Router>
-        <App store={store} >
-            <Route exact path="/" component={Index} />
-            <Route path="/todo" component={Todo} />
-            <Route exact path="/todoAdd" component={TodoForm} />
-        </App>
-    </Router>
+    <Provider  store={store} todoStore={todoStore}>
+        <Router>
+            <LocaleProvider locale={zh_CN}>
+                <App>
+                    <Route exact path={RouterVar.Home} component={Index} />
+                    <Route path={RouterVar.TodoRouter.TODO} component={Todo} />
+                    <Route exact path={RouterVar.TodoRouter.TODOADD} component={TodoForm} />
+                </App>
+            </LocaleProvider>
+        </Router>
+    </Provider>
     , document.getElementById('root'));
--- src/view/todo/TodoForm.js
+++ src/view/todo/TodoForm.js
@@ -2,18 +2,44 @@ import React, { Component } from 'react'
 import {
     Card,Form, Input, Button,DatePicker
   } from 'antd';
-const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+const { RangePicker} = DatePicker;
+import { observer, inject } from 'mobx-react';
 
-export default class TodoForm extends Component {
+@inject("todoStore")
+@observer
+
+ class TodoForm extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      todo:'',
+      todoDate:[]
+    }
+  }
+
+  handleChange=(e)=>{
+    this.setState({
+      [e.target.name] : e.target.value
+    })
+  }
+  handleChangeDate=(dates,dateStrings)=>{
+    this.setState({
+      todoDate:this.state.todoDate.push(dateStrings)
+    })
+  }
+  handleAddTodo=()=>{
+    this.props.todoStrore.Add(this.state);
+  } 
+
   render() {
     return (
         <Card title="添加待办事项">
         <Form>
           <Form.Item>
-            <Input placeholder="输入代办事项" />
+            <Input placeholder="输入代办事项" name="todo" value={this.state.todo} onChange={this.handleChange.bind(this)}/>
           </Form.Item>
           <Form.Item>
-            <RangePicker size={'default'} showTime />
+            <RangePicker showTime onChange={this.handleChangeDate.bind(this)}/>
           </Form.Item>
           <Form.Item>
             <Button type="primary" onClick={() => this.handleAddTodo()} >新增代办事项</Button>
@@ -23,3 +49,4 @@ export default class TodoForm extends Component {
     )
   }
 }
+export default TodoForm;
